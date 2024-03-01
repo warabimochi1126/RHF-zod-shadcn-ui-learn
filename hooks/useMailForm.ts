3bfad1f2f320d9ae7f20395efe5b8c50384bc59a
warabimochi <satomi920003@yahoo.co.jp>
2024-03-01 20:33:07 +0900

    18

--- hooks/useMailForm.ts
+++ hooks/useMailForm.ts
@@ -15,8 +15,14 @@ export const useMailForm = () => {
       });
     
     
-    const onSubmit = useCallback((values: any) => {
-        console.log(values);
+    const onSubmit = useCallback(async (values: any) => {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/send`, {
+          method: "POST"
+        })
+      } catch(error) {
+        console.error(error);
+      }
     }, []);
 
     return { form, onSubmit };
