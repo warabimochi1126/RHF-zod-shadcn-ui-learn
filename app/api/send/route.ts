40ce9f6cc5862da71ace6900557acb3a57410caf
warabimochi <satomi920003@yahoo.co.jp>
2024-03-03 01:23:17 +0900

    27

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,8 +5,21 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-    const { username, subject, email, content, file } = await request.json();
-    
+    // const { username, subject, email, content, file } = await request.json();    
+    // console.log(username, subject, email, content, file);
+
+    const formData = await request.formData();
+
+    const username = formData.get("username") as string;
+    const email = formData.get("email") as string;
+    const subject = formData.get("subject") as string;
+    const content = formData.get("content") as string;
+    const file = formData.get("file") as File;
+
+    console.log(file);
+    const buffer =  Buffer.from(await file.arrayBuffer());
+    console.log(buffer);
+
     try {
         const { data, error } = await resend.emails.send({
             from:  'Acme <onboarding@resend.dev>',
@@ -19,7 +32,7 @@ export async function POST(request: Request) {
             }) as React.ReactElement,
             attachments: [{
                 filename: file.name,
-                content: file
+                content: buffer
             }]
         });
 
--- hooks/useMailForm.ts
+++ hooks/useMailForm.ts
@@ -2,9 +2,10 @@ import { formSchema } from "@/lib/formSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useCallback } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export const useMailForm = () => {
-    const form = useForm({
+    const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
           username: "",
@@ -15,22 +16,22 @@ export const useMailForm = () => {
         }
       });
     
-    const onSubmit = useCallback(async (values: any) => {
+    const onSubmit = useCallback(async (values: z.infer<typeof formSchema>) => {
       const { username, email, subject, content, file } = values;
 
+      const formData = new FormData();
+      formData.append("username", username);
+      formData.append("email", email);
+      formData.append("subject", subject);
+      formData.append("content", content);
+      formData.append("file", file[0]);
+
+      console.log(username, subject, email, content, file);
 
       try {
         await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/send`, {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            username,
-            email,
-            subject,
-            content
-          })
+          body: formData
         })
       } catch(error) {
         console.error(error);
