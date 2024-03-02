ab1de10d30620bb5e848ef5e27e03061c44d162b
warabimochi <satomi920003@yahoo.co.jp>
2024-03-02 20:50:53 +0900

    21

--- lib/formSchema.ts
+++ lib/formSchema.ts
@@ -4,6 +4,6 @@ export const formSchema = z.object({
     username: z.string().min(2, { message: "ユーザー名は2文字以上で入力してください。"}),
     subject: z.string().min(2, { message: "主題は2文字以上で入力してください。" }),
     email: z.string().email({ message: "適切なメールアドレスを入力してください。" }),
-    content: z.string().min(10, { message: "本文は10文字以上で入力してください。" }).max(160, { message: "本文は160文字以内で入力してください。" })
-    
+    content: z.string().min(10, { message: "本文は10文字以上で入力してください。" }).max(160, { message: "本文は160文字以内で入力してください。" }),
+    file: z.custom<FileList>().refine((files) => files?.length !== 0, "ファイル画像が必要です。")
 })
