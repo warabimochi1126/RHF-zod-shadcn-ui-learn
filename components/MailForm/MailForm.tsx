f2d4c8a8bddd2b50712043e445cf8ef10e3cb62f
warabimochi <satomi920003@yahoo.co.jp>
2024-03-03 02:56:28 +0900

    29

--- components/MailForm/MailForm.tsx
+++ components/MailForm/MailForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
@@ -10,12 +10,22 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Textarea } from '../ui/textarea';
 import { useMailForm } from '@/hooks/useMailForm';
 import { ClipLoader } from 'react-spinners';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const MailForm = () => {
   const { form, onSubmit } = useMailForm();
 
+  useEffect(() => {
+    if (form.formState.isSubmitSuccessful) {
+      toast.success("メール送信に成功しました！");
+    }
+  }, [form.formState.isSubmitSuccessful]);
+  
   return (
     <Form {...form}>
+    <ToastContainer />
     <form onSubmit={form.handleSubmit(onSubmit)} className="container flex flex-col gap-3">
       <FormField
         control={form.control}
